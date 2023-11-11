08981dca7254fef314fd623fdb7c243897f539d4
Utouchmytalala <138011822+poommy0603@users.noreply.github.com>
2023-11-11 17:50:41 +0700

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,8 @@
 function imgSlider(anything) {
     document.querySelector('.pepsi').src = anything;
 }
+
+function changebgcolor(color) {
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
